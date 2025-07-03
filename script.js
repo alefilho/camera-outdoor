5bce8df6b26e00264b2bba57438de696a326c227
Alessandro Rocha <alerocha.dev@hotmail.com>
2025-07-03 18:06:21 -0300

    feat: initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const { exec } = require('child_process');
+const fs = require('fs');
+const axios = require('axios');
+
+const rtspUrl = process.env.RTSP_URL;
+const snapshotFile = process.env.SNAPSHOT_FILE;
+const wahaApiUrl = process.env.WAHA_API_URL;
+const wahaApiKey = process.env.WAHA_API_KEY;
+const wahaSession = process.env.WAHA_SESSION;
+const wahaChatId = process.env.WAHA_CHAT_ID;
+
+const ffmpegCmd = `ffmpeg -rtsp_transport tcp -i "${rtspUrl}" -frames:v 1 -q:v 2 ${snapshotFile}`;
+
+exec(ffmpegCmd, (error) => {
+  if (error) {
+    console.error(`Erro ao capturar snapshot: ${error.message}`);
+    return;
+  }
+
+  console.log('Snapshot salvo!');
+
+  const imageBuffer = fs.readFileSync(snapshotFile);
+  const imageBase64 = imageBuffer.toString('base64');
+
+  const payload = {
+    session: wahaSession,
+    chatId: wahaChatId,
+    file: {
+      mimetype: 'image/jpeg',
+      filename: snapshotFile,
+      data: imageBase64
+    },
+    caption: `${new Date().toLocaleString()}`
+  };
+
+  axios.post(wahaApiUrl, payload, {
+    headers: {
+      'x-api-key': wahaApiKey
+    }
+  })
+  .then((response) => {
+    console.log('Imagem enviada com sucesso');
+  })
+  .catch((err) => {
+    console.error('Erro ao enviar imagem:', err.response?.data || err.message);
+  })
+  .finally((end) => {
+    if (fs.existsSync(snapshotFile)) {
+        fs.unlinkSync(snapshotFile);
+        console.log(`Arquivo ${snapshotFile} deletado!`);
+      }
+  });
+});
